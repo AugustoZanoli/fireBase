fb93e45980d357575d9a2acffd0070f92a6e0dbf
Zanoli <zancamar9@gmail.com>
2023-11-02 17:47:29 -0300

    "Criação da aba de busca"

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:chat/pages/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 import '../components/to_do_form.dart';
@@ -11,7 +13,7 @@ class DoPage extends StatefulWidget {
 }
 
 class _DoPageState extends State<DoPage> {
-  final List<ToDo> _ToDos = [];
+  List<ToDo> _ToDos = [];
 
   List<ToDo> get _recentToDos {
     return _ToDos.where((tr) {
@@ -19,6 +21,26 @@ class _DoPageState extends State<DoPage> {
         Duration(days: 7),
       ));
     }).toList();
+  }
+
+  List<ToDo> searchToDosByName(List<ToDo> todos, String query) {
+    query = query.toLowerCase();
+    return todos
+        .where((todo) => todo.name.toLowerCase().contains(query))
+        .toList();
+  }
+
+  List<ToDo> filterToDosByCategory(List<ToDo> todos, String category) {
+    category = category.toLowerCase();
+    return todos
+        .where((todo) => todo.category.toLowerCase() == category)
+        .toList();
+  }
+
+  void updateToDos(List<ToDo> toDos) {
+    setState(() {
+      _ToDos = toDos;
+    });
   }
 
   _addToDo(String name, String category, DateTime date) {
@@ -53,12 +75,25 @@ class _DoPageState extends State<DoPage> {
     );
   }
 
+  void _handleSearch(String query) {
+    final TextEditingController _searchController = TextEditingController();
+    // Use o valor do controlador para realizar a pesquisa.
+    final searchResults = searchToDosByName(_ToDos, _searchController.text);
+    // Atualize o estado do widget com os resultados da pesquisa.
+    setState(() {
+      _ToDos = searchResults;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final authService = AuthService();
+    final currentUser = authService.currentUser;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Text('ToDo'),
+        title: Text('Minha Lista de Afazeres'),
         titleTextStyle: TextStyle(
           color: Colors.white,
           fontWeight: FontWeight.bold,
@@ -66,38 +101,48 @@ class _DoPageState extends State<DoPage> {
         ),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _openToDoFormModal(context),
+            icon: Icon(Icons.search),
+            color: Colors.white,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SearchScreen(onSearch: _handleSearch),
+                ),
+              );
+            },
           ),
-          DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Theme.of(context).primaryIconTheme.color,
-            ),
-            items: [
-              DropdownMenuItem(
-                value: 'logout',
-                child: Container(
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.exit_to_app,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text('Sair'),
-                    ],
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  value: 'logout',
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.exit_to_app,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('Sair'),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
-            onChanged: (value) {
-              if (value == 'logout') {
-                AuthService().logout();
-              }
-            },
+              ],
+              onChanged: (value) {
+                if (value == 'logout') {
+                  AuthService().logout();
+                }
+              },
+            ),
           )
         ],
       ),
@@ -105,25 +150,56 @@ class _DoPageState extends State<DoPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: 60,
-              width: 10,
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
-                      ),
-                      color: Color.fromRGBO(220, 220, 220, 1),
-                      borderRadius: BorderRadius.circular(5),
+            Card(
+              color: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .spaceBetween, // Coloca os elementos no canto direito e esquerdo
+                  children: [
+                    Row(
+                      children: [
+                        // Aqui você pode exibir a imagem do usuário (se disponível)
+                        if (currentUser != null && currentUser.imageURL != null)
+                          CircleAvatar(
+                            backgroundImage:
+                                FileImage(File(currentUser.imageURL)),
+                            radius:
+                                25, // Ajuste o tamanho do avatar conforme necessário
+                          ),
+
+                        // Nome e e-mail do usuário
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Olá, ${currentUser?.name ?? 'Usuário'}!',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                'E-mail: ${currentUser?.email ?? ''}',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
+
             ToDoList(_ToDos, _removeToDo),
             // Por exemplo, sua lista de afazeres
           ],
---
+++ lib/pages/search_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'chat_page.dart';
+
+class SearchScreen extends StatelessWidget {
+  final void Function(String) onSearch;
+  final TextEditingController _searchController = TextEditingController();
+
+  SearchScreen({required this.onSearch});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pesquisar Afazeres'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _searchController, // Use o TextEditingController aqui
+              onChanged: (query) {
+                // Neste ponto, o valor da pesquisa é atualizado automaticamente no controlador.
+              },
+              decoration: InputDecoration(labelText: 'Pesquisar por nome'),
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                final query =
+                    _searchController.text; // Obtenha o valor do campo de texto
+                onSearch(
+                    query); // Chame a função de pesquisa com o valor obtido
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
